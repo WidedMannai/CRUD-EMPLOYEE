e44e28f0f49399ccd3350b09b5b0c59e02b5cb1a
WidedMannai <72729292+WidedMannai@users.noreply.github.com>
2020-11-23 11:11:39 +0100

    Add files via upload

---
+++ CRUDEmployee/src/main/java/org/sid/Employee.java
@@ -0,0 +1,76 @@
+package org.sid;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+	 @Id
+	 @GeneratedValue(strategy = GenerationType.AUTO)
+	 private long id;
+	 
+	 @Column(name = "firstName", nullable = false)
+	 private String firstName;
+	 
+	 @Column(name = "lastName", nullable = false)
+	 private String lastName;
+	 
+	 @Column(name = "emailAddress", nullable = false)
+	    private String emailId;
+	 
+	 
+	    public Employee() {
+	  	  
+	    }
+	 
+	    public Employee(String firstName, String lastName, String emailId) {
+	         this.firstName = firstName;
+	         this.lastName = lastName;
+	         this.emailId = emailId;
+	    }
+	 
+	   
+	        public long getId() {
+	        return id;
+	    }
+	    public void setId(long id) {
+	        this.id = id;
+	    }
+	 
+	    
+	    public String getFirstName() {
+	        return firstName;
+	    }
+	    public void setFirstName(String firstName) {
+	        this.firstName = firstName;
+	    }
+	 
+	   
+	    public String getLastName() {
+	        return lastName;
+	    }
+	    public void setLastName(String lastName) {
+	        this.lastName = lastName;
+	    }
+	 
+	    
+	    public String getEmailId() {
+	        return emailId;
+	    }
+	    public void setEmailId(String emailId) {
+	        this.emailId = emailId;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", emailId=" + emailId
+	       + "]";
+	    }
+	 
+	
+}
